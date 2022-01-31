8f3e16785add8fab299a49198a09b0c554c52bac
andreaskalikatzaros <98468010+andreaskalikatzaros@users.noreply.github.com>
2022-01-31 19:15:01 +0200

    Update ask7.py

--- ask7.py
+++ ask7.py
@@ -1,32 +1,74 @@
 from collections import Counter
+import json
+from os import waitpid
+import pprint
 d={}
-y={}
-with open("dictionary.txt", "r") as f:
-    for line in f:
-        (key,val,val2,val3,val4,val5,val6,val7)=line.split(",")
-        d[key]=val,val2,val3,val4,val5,val6,val7
+e={}
+p=[]
+with open('dictionary.json') as json_file:
+    data = json.load(json_file)
+dictionary_items = data.items()
+k=data["cars"]
+my_dict={}
+for index,value in enumerate(k):
+  my_dict[index] = value
 
-keys = d.keys()
-print(keys)
 
-dictionary_items = d.items()
+u=my_dict.keys()
+print(my_dict)
+y=my_dict[1].keys()
+print(y)
 
-va = input("Enter one of the names: ")
+for i in range(len(y)):
+               p1=list(my_dict[1].keys())[i]
+               p.append(p1)
+max1=my_dict[0]['hp']
+mod1max=my_dict[0]['model']
+min1=my_dict[0]['hp']
+mod1min=my_dict[0]['model']
+mod2max=my_dict[0]['model']
+mod2min=my_dict[0]['model']
+max2=my_dict[0]['year']
+min2=my_dict[0]['year']
+onomax=my_dict[0]['model']
+onomin=my_dict[0]['model']
 
- 
+for i in range (len(u)):
+    q=my_dict[i]['hp']
+    a=my_dict[i]['year']
+    w=my_dict[i]['model']
+    if q>max1:
+        max1=my_dict[i]['hp']
+        mod1max=my_dict[1]['model']
+    if q<min1:
+        min1=my_dict[i]['hp']
+        mod1min=my_dict[i]['model']
+    if a>max2:
+        mod2max=my_dict[i]['model']
+        max2=my_dict[i]['year']
+    if a<min2:
+        mod2min=my_dict[i]['model']
+        min2=my_dict[i]['year']
+    if w>onomax:
+        onomax=w 
+    if w<onomin:
+        onomin=w
+va = input("dwse model(modelo) h year(etos kataskevis) h hp(aloga).Se periptosi poy dwseis model ta apotelesmata emfanizontai alfavitika: : ")
+
+
 search_key = va
- 
-if search_key in d: 
-    print(d.get(va))
-    y=d.get(va)
-    print("to megalutero stoixeio einai",max(y, key=int))
-    print("to mikrotero stoixeio einai",min(y, key=int))
-    counts = Counter(y)
-    top = counts.most_common(1)
-    print("to stoixeio poy emfanistike tis perisoteres fores eina to",top,"fores emfanistike")
-    
-
-
-          
-else: 
-     print("The key does not exist in the dictionary.") 
+if search_key in p:
+  for i in range (len(p)):
+        if search_key==p[i]:
+            pos=i
+            print (pos)
+  if pos==2:
+            print("to palaiotero amaksi einai to", mod2min,"me xronologia",min2)
+            print("to neotero amaksi einai to", mod2max,"me xronologia",max2)
+  if pos==1:
+            print("to pio argo amaksi einai to ",mod1min,"me ",min1," aloga ")
+            print("to pio grigoro amaksi einai to ",mod1max,"me ",max1," aloga ")
+  if pos==0:
+            print("me alfavitiki seira prwto einai to ",onomax," kai teleutaio einai to ",onomin)
+else:
+    print("den edwses sosto kleidi")
